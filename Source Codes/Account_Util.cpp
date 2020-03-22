61a41d226e8aa9d5fb4634b5c5b1c9adda325776
Rishi K <59014665+CODESofRishi@users.noreply.github.com>
2020-03-22 20:22:56 +0530

    Utility helper implementation for the hierarchy

---
+++ Source Codes/Account_Util.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "Account_Util.h"
+
+// Displays abstract Account objects in a vector of Account objects
+void display(const std::vector<Account*> &accounts) {
+    std::cout<<"\n==========Accounts=================================================="<<std::endl;
+    for (auto const &acc: accounts)
+        std::cout<<*acc<<std::endl;
+}
+
+
+// Deposits supplied amount to each abstract Account object in the vector
+void deposit(std::vector<Account*> &accounts, double amount) {
+    std::cout<<"\n==========Depositing to Accounts===================================="<<std::endl;
+    for (auto &acc: accounts) {
+        if (acc->deposit(amount))
+            std::cout<<"Deposited "<<amount<<" to "<<*acc<<std::endl;
+        else
+            std::cout<<"Failed deposit of "<<amount<<" to "<<*acc<<std::endl;
+    }
+}
+
+// Withdraw amount from each abstract Account object in the vector
+void withdraw(std::vector<Account*> &accounts, double amount) {
+    std::cout<<"\n==========Withdrawing from Accounts=================================="<<std::endl;
+    for (auto &acc: accounts) {
+        if (acc->withdraw(amount))
+            std::cout<<"Withdrew "<<amount<<" from "<<*acc<<std::endl;
+        else
+            std::cout<<"Failed withdrawal of "<<amount<<" from "<<*acc<<std::endl;
+    }
+}
