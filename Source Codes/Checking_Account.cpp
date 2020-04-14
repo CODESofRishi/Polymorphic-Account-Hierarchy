870b9f087a511e07e69299ab1549ddfc28c028f7
Rishi K <59014665+CODESofRishi@users.noreply.github.com>
2020-04-14 12:20:16 +0530

    Update Checking_Account.cpp

--- Source Codes/Checking_Account.cpp
+++ Source Codes/Checking_Account.cpp
@@ -1,26 +1,18 @@
-#ifndef _CHECKING_ACCOUNT_H_
-#define _CHECKING_ACCOUNT_H_
 #include <string>
-#include "Account.h"
+#include "Checking_Account.h"
 
-class Checking_Account: public Account {
-private:
-    static constexpr double def_fee {0.0};
-    static constexpr double def_balance {0.0};
-    static constexpr const char *def_name {"None"};
-    static double inspect_fee(const double &fee_amnt);
-    static double inspect_balance(const double &bal);
+Checking_Account::Checking_Account(const std::string &name, const double &amount, const double &fee)
+    :Account(name, amount), fee {fee} {}
 
-protected:
-    double fee;
+bool Checking_Account::withdraw(const double &amount) {
+    if (amount < 0) return false;
+    return Account::withdraw(amount+fee);
+}
 
-public:
-    Checking_Account(const std::string &name = def_name, const double &amount = def_balance, const double &fee = def_fee);
-    virtual bool withdraw(const double &amount) override;
-    virtual bool deposit(const double &amount) override;
-    virtual void print(std::ostream &os) const override;
+bool Checking_Account::deposit(const double &amount) {
+    return Account::deposit(amount);
+}
 
-    virtual ~Checking_Account() = default;
-};
-
-#endif // _CHECKING_ACCOUNT_H_
+void Checking_Account::print(std::ostream &os) const {
+    os << "[Checking Account: " << name << " Balance: " << balance << " Fee: " << fee <<"]";
+}
